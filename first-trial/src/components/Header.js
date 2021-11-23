7b46037c6dd8669b3e0725b40ea01b73ae3919ed
hasanarcas <hasanarcas@gmail.com>
2021-11-23 16:35:45 +0300

    Updated App.js

--- first-trial/src/components/Header.js
+++ first-trial/src/components/Header.js
@@ -1,7 +1,15 @@
-const Header = (props) => {
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({title}) => {
+    const onClick = () => {
+        console.log('Click');
+    }
+
     return (
-        <header>
-            <h1>{props.title}</h1>
+        <header className= 'header'>
+            <h1>{title}</h1>
+            <Button color = 'green' text="Add" onClick={onClick}/>
         </header>
     )
 }
@@ -10,5 +18,8 @@ Header.defaultProps = {
     title: 'Task Tracker',
 }
 
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
 
 export default Header
