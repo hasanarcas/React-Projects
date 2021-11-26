c65e429ba434e807ec1a49eef18832b90521fbef
hasanarcas <hasanarcas@gmail.com>
2021-11-26 17:35:44 +0300

    Updated App.js and created new components

---
+++ blog-with-react/src/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return (
+        <div>
+            <nav className="navbar">
+                <h1>The Arcas Blog</h1>
+                <div className="links">
+                    <a href="/">Home</a>
+                    <a href="/create">New Blog</a>
+                </div>
+            </nav>
+
+        </div>
+    )
+}
+
+export default Navbar
