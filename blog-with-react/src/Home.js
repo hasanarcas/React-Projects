c92f43a6d270a512bdde8e06aeed69a5a5c9d9ca
hasanarcas <hasanarcas@gmail.com>
2021-11-27 23:36:31 +0300

    Added json server for blog website

--- blog-with-react/src/Home.js
+++ blog-with-react/src/Home.js
@@ -1,23 +1,28 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import BlogList from "./BlogList";
 
 const Home = () => {
-    const [name, setName] = useState("hasan");
-    const [age, setAge] = useState(25)
+    const [blogs, setBlogs] = useState([
+        {title: "Blog 1", body: "lorem ipsum...", author:"Hasan", id :1},
+        {title: "Blog 2", body: "lorem ipsum...", author:"Hakan", id :2},
+        {title: "Blog 3", body: "lorem ipsum...", author:"Hasan", id :3}
+    ]);
 
 
-
-    const handleClick = () =>{
-        setName("Hakan")
-        setAge(500)
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter((blog) => blog.id !== id)
+        setBlogs(newBlogs);
     }
+
+    useEffect(() => {
+        
+    }, []);
 
     return (
         <div className="home">
-            <h2>HomePage</h2>
-            <p>{name} is {age} years old.</p>
-            <button onClick={handleClick}>Click Me</button>
+            <BlogList blogs={blogs} title ="All Blogs!"  handleDelete={handleDelete}/>
         </div>
-    )
+    );
 }
 
 export default Home
