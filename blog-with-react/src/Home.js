7927251459469bc75a4029a047f062af4ff3098c
hasanarcas <hasanarcas@gmail.com>
2021-11-28 11:41:27 +0300

    Created fetch method to take data and error handling with catch

--- blog-with-react/src/Home.js
+++ blog-with-react/src/Home.js
@@ -2,26 +2,30 @@ import { useState, useEffect } from "react"
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {title: "Blog 1", body: "lorem ipsum...", author:"Hasan", id :1},
-        {title: "Blog 2", body: "lorem ipsum...", author:"Hakan", id :2},
-        {title: "Blog 3", body: "lorem ipsum...", author:"Hasan", id :3}
-    ]);
-
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter((blog) => blog.id !== id)
-        setBlogs(newBlogs);
-    }
+    const [blogs, setBlogs] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        
+      setTimeout(() => {
+        fetch("http://localhost:8000/blogs")
+        .then(res => {
+          return res.json()
+        })
+        .then((data) => {
+          setBlogs(data)
+          setIsLoading(false)
+        })
+        .catch(err => {
+            console.log(err.message);
+        })
+      }, 1000)
     }, []);
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title ="All Blogs!"  handleDelete={handleDelete}/>
-        </div>
+            {isLoading && <div>Loading ...</div>}
+            {blogs && <BlogList blogs={blogs} title ="All Blogs!"/>}
+        </div>  
     );
 }
 
