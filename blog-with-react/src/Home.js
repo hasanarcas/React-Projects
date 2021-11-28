844a36dadcc4aec70b56003c60588a3aba6de8da
hasanarcas <hasanarcas@gmail.com>
2021-11-28 15:16:21 +0300

    Created new pages under the root page

--- blog-with-react/src/Home.js
+++ blog-with-react/src/Home.js
@@ -1,28 +1,12 @@
-import { useState, useEffect } from "react"
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
-
-    useEffect(() => {
-      setTimeout(() => {
-        fetch("http://localhost:8000/blogs")
-        .then(res => {
-          return res.json()
-        })
-        .then((data) => {
-          setBlogs(data)
-          setIsLoading(false)
-        })
-        .catch(err => {
-            console.log(err.message);
-        })
-      }, 1000)
-    }, []);
+    const {data: blogs, isLoading, error} = useFetch("http://localhost:8000/blogs")
 
     return (
         <div className="home">
+            {error && <div>{error}</div>}
             {isLoading && <div>Loading ...</div>}
             {blogs && <BlogList blogs={blogs} title ="All Blogs!"/>}
         </div>  
